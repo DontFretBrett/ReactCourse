fcf9f5d3ba6ae5d9cbeb58c3957b1c522b4b4200
Brett Sanders <blogbrett@gmail.com>
2024-07-20 16:03:20 -0700

    Dismissable alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
 import RButton from './components/RButton';
+import { useState } from 'react';
 
 function App() {
   const items = ['New York', 'San Francisco', 'Tokyo', 'Paris'];
+
+  const [alertDisplayed, displayAlert] = useState(false);
 
   const handleSelectedItem = (item: string) => {
     console.log(item);
   };
 
-  const handleButtonClick = () => {
-    console.log('Clicked');
-  };
-
   return (
-    <div>
+    <>
       <ListGroup
         items={items}
         heading="Cities"
         onSelectedItem={handleSelectedItem}
       />
-      <Alert>
-        Hello <strong>World</strong>
-      </Alert>
-      <RButton onClick={handleButtonClick} color="success">Submit</RButton>
-    </div>
+      {alertDisplayed && (
+        <Alert onClose={() => displayAlert(false)}>
+          Hello <strong>World</strong>
+        </Alert>
+      )}
+      <RButton onClick={() => displayAlert(true)} color="success">
+        Show Alert
+      </RButton>
+    </>
   );
 }
 
