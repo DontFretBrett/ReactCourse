9dc6c78be6e201517020c8eeda489e54f66fe114
Brett Sanders <blogbrett@gmail.com>
2024-07-20 15:30:33 -0700

    ReactNode (like content projection in Angular)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
 
 function App() {
   const items = ['New York', 'San Francisco', 'Tokyo', 'Paris'];
 
   const handleSelectedItem = (item: string) => {
     console.log(item);
-  }
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectedItem={handleSelectedItem} />
-      </div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectedItem={handleSelectedItem}
+      />
+      <Alert>Hello <strong>World</strong></Alert>
+    </div>
   );
 }
 
