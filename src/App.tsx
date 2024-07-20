1b1d1db6e2a14fff11eab91578bc134be0d98fcb
Brett Sanders <blogbrett@gmail.com>
2024-07-20 15:51:53 -0700

    Add RButton, union type for color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
+import RButton from './components/RButton';
 
 function App() {
   const items = ['New York', 'San Francisco', 'Tokyo', 'Paris'];
 
   const handleSelectedItem = (item: string) => {
     console.log(item);
+  };
+
+  const handleButtonClick = () => {
+    console.log('Clicked');
   };
 
   return (
@@ -15,7 +20,10 @@ function App() {
         heading="Cities"
         onSelectedItem={handleSelectedItem}
       />
-      <Alert>Hello <strong>World</strong></Alert>
+      <Alert>
+        Hello <strong>World</strong>
+      </Alert>
+      <RButton onClick={handleButtonClick} color="success">Submit</RButton>
     </div>
   );
 }
