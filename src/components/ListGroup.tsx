e38f74c254fcbc59437263c01e9f206aa5997e74
Brett Sanders <blogbrett@gmail.com>
2024-07-20 15:14:14 -0700

    Add ListGroup properties

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 
-import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  const items = ['New York', 'San Francisco', 'Tokyo', 'Paris'];
+function ListGroup({items, heading}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={item} className={index === selectedIndex ? "list-group-item active" : "list-group-item"} 
-          onClick={() => setSelectedIndex(index)}>
+          <li
+            key={item}
+            className={
+              index === selectedIndex
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
